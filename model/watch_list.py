b622e57a9eb4b9c60459e8ced6b7be1f6701965b
DHarshana Ratnayake <dharshana.ratnayake@turners.co.nz>
2024-07-01 15:30:34 +1200

    fix fieldInfo error

--- model/watch_list.py
+++ model/watch_list.py
@@ -1,7 +1,14 @@
+from typing import List
+
 from pydantic.v1 import BaseModel, Field
+
+
+class Comment(BaseModel):
+    vehicles: str = Field("vehicle source this comment is for")
+    comment: str = Field("the comment")
 
 
 class WatchList(BaseModel):
     user_id: str = Field("Unique identifier for this user this watch list belongs to")
-    vehicles: list[str] = Field("List of vehicle sources that are on this watch list")
-    comments: dict[str, str] = Field("List of vehicle sources that are on this watch list")
+    vehicles: List[str] = Field("List of vehicle sources that are on this watch list")
+    comments: List[Comment] = Field("List of vehicle sources that are on this watch list")
