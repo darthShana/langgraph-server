3a0ad178e66a7ecc4b35bad76931b5db3e46b693
DHarshana Ratnayake <dharshana.ratnayake@turners.co.nz>
2024-07-01 16:35:56 +1200

    change to user id

--- model/user_profile.py
+++ model/user_profile.py
@@ -6,6 +6,6 @@ from model.vehicle_listing import LocationEnum
 
 
 class UserProfile(BaseModel):
-    id: str = Field("Unique identifier for this user")
+    user_id: str = Field("Unique identifier for this user")
     name: str = Field("The name the the user would like to be called")
     preferred_branches: List[LocationEnum] = Field("List of branches the user is looking for a vehicle in")
